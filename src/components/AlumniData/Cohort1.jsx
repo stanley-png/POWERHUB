80e24d0510608a0154bb38afad35e85e48cf66a6
Evans-mutuku <evansnyamai98@gmail.com>
2023-06-19 09:09:14 +0300

    building alumni section

---
+++ src/components/AlumniData/Cohort1.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+
+const Cohort1 = () => {
+  return (
+    <main>
+      <div class="max-w-5xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        {/* <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+          <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+            Our team
+          </h2>
+          <p class="mt-1 text-gray-600 dark:text-gray-400">Creative people</p>
+        </div> */}
+
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 md:gap-12">
+          <div class="text-center">
+            <img
+              class="rounded-full w-24 h-24 mx-auto"
+              src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png"
+              alt="Image Description"
+            />
+            <div class="mt-2 sm:mt-4">
+              <h3 class="font-medium text-gray-800">Dedan Okware</h3>
+            </div>
+          </div>
+
+          <div class="text-center">
+            <img
+              class="rounded-full w-24 h-24 mx-auto"
+              src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png"
+              alt="Image Description"
+            />
+            <div class="mt-2 sm:mt-4">
+              <h3 class="font-medium text-gray-800">Univer Omoro</h3>
+            </div>
+          </div>
+
+          <div class="text-center">
+            <img
+              class="rounded-full w-24 h-24 mx-auto"
+              src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png"
+              alt="Image Description"
+            />
+            <div class="mt-2 sm:mt-4">
+              <h3 class="font-medium text-gray-800 ">Kendi Nceene</h3>
+            </div>
+          </div>
+
+          <div class="text-center">
+            <img
+              class="rounded-full w-24 h-24 mx-auto"
+              src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png"
+              alt="Image Description"
+            />
+            <div class="mt-2 sm:mt-4">
+              <h3 class="font-medium text-gray-800 ">Laban Deya</h3>
+            </div>
+          </div>
+
+          <div class="text-center">
+            <img
+              class="rounded-full w-24 h-24 mx-auto"
+              src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png"
+              alt="Image Description"
+            />
+            <div class="mt-2 sm:mt-4">
+              <h3 class="font-medium text-gray-800 ">Ali Jeffar</h3>
+            </div>
+          </div>
+
+          <div class="text-center">
+            <img
+              class="rounded-full w-24 h-24 mx-auto"
+              src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png"
+              alt="Image Description"
+            />
+            <div class="mt-2 sm:mt-4">
+              <h3 class="font-medium text-gray-800 ">Eric Muriithi</h3>
+            </div>
+          </div>
+
+          <div class="text-center">
+            <img
+              class="rounded-full w-24 h-24 mx-auto"
+              src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png"
+              alt="Image Description"
+            />
+            <div class="mt-2 sm:mt-4">
+              <h3 class="font-medium text-gray-800 ">Thermister</h3>
+            </div>
+          </div>
+
+          <div class="text-center">
+            <img
+              class="rounded-full w-24 h-24 mx-auto"
+              src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png"
+              alt="Image Description"
+            />
+            <div class="mt-2 sm:mt-4">
+              <h3 class="font-medium text-gray-800 ">Jeff Fisher</h3>
+            </div>
+          </div>
+
+          <div class="text-center">
+            <img
+              class="rounded-full w-24 h-24 mx-auto"
+              src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png"
+              alt="Image Description"
+            />
+            <div class="mt-2 sm:mt-4">
+              <h3 class="font-medium text-gray-800 ">Sophia Harrington</h3>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-12 flex justify-center">
+          <div class="border border-gray-200 p-1.5 pl-5 rounded-full">
+            <div class="flex items-center gap-x-3">
+              <span class="text-sm text-gray-500">Want to learn with us?</span>
+              <a
+                class="inline-flex justify-center items-center gap-x-2 text-center bg-white border hover:border-gray-300 text-sm text-[#C1224F] hover:text-[#13ABC4] font-medium hover:shadow-sm rounded-full focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 "
+                href="#"
+              >
+                Join Next Cohort
+                <svg
+                  class="w-2.5 h-2.5"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                >
+                  <path
+                    d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Cohort1;
--- src/components/StaticPages/Alumni.jsx
+++ src/components/StaticPages/Alumni.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import Cohort1 from "../AlumniData/Cohort1";
 
 const Alumni = () => {
   return (
     <main>
       <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-          <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+          <h2 class="text-2xl font-bold md:text-4xl md:leading-tight ">
             Our Alumni
           </h2>
-          <p class="mt-1 text-gray-600 dark:text-gray-400">Creative people</p>
+          <p class="mt-1 text-gray-600 ">Creative people</p>
         </div>
 
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div class="flex flex-col rounded-xl p-4 md:p-6 bg-white border border-gray-200 dark:bg-slate-900 dark:border-gray-700">
+          <div class="flex flex-col rounded-xl p-4 md:p-6 bg-white border border-gray-200 ">
             <div class="flex items-center gap-x-4">
               <img
                 class="rounded-full w-20 h-20"
@@ -35,7 +36,7 @@ const Alumni = () => {
 
             <div class="mt-3 space-x-1">
               <a
-                class="inline-flex justify-center items-center text-gray-500 border border-gray-200 w-8 h-8 rounded-md hover:text-gray-800 hover:shadow-sm dark:hover:text-gray-200 dark:border-gray-700 dark:hover:shadow-slate-700/[.7]"
+                class="inline-flex justify-center items-center text-gray-500 border border-gray-200 w-8 h-8 rounded-md hover:text-gray-800 hover:shadow-sm "
                 href="#"
               >
                 <svg
@@ -50,7 +51,7 @@ const Alumni = () => {
                 </svg>
               </a>
               <a
-                class="inline-flex justify-center items-center text-gray-500 border border-gray-200 w-8 h-8 rounded-md hover:text-gray-800 hover:shadow-sm dark:hover:text-gray-200 dark:border-gray-700 dark:hover:shadow-slate-700/[.7]"
+                class="inline-flex justify-center items-center text-gray-500 border border-gray-200 w-8 h-8 rounded-md hover:text-gray-800 hover:shadow-sm"
                 href="#"
               >
                 <svg
@@ -64,25 +65,10 @@ const Alumni = () => {
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                 </svg>
               </a>
-              <a
-                class="inline-flex justify-center items-center text-gray-500 border border-gray-200 w-8 h-8 rounded-md hover:text-gray-800 hover:shadow-sm dark:hover:text-gray-200 dark:border-gray-700 dark:hover:shadow-slate-700/[.7]"
-                href="#"
-              >
-                <svg
-                  class="w-3.5 h-3.5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M3.362 10.11c0 .926-.756 1.681-1.681 1.681S0 11.036 0 10.111C0 9.186.756 8.43 1.68 8.43h1.682v1.68zm.846 0c0-.924.756-1.68 1.681-1.68s1.681.756 1.681 1.68v4.21c0 .924-.756 1.68-1.68 1.68a1.685 1.685 0 0 1-1.682-1.68v-4.21zM5.89 3.362c-.926 0-1.682-.756-1.682-1.681S4.964 0 5.89 0s1.68.756 1.68 1.68v1.682H5.89zm0 .846c.924 0 1.68.756 1.68 1.681S6.814 7.57 5.89 7.57H1.68C.757 7.57 0 6.814 0 5.89c0-.926.756-1.682 1.68-1.682h4.21zm6.749 1.682c0-.926.755-1.682 1.68-1.682.925 0 1.681.756 1.681 1.681s-.756 1.681-1.68 1.681h-1.681V5.89zm-.848 0c0 .924-.755 1.68-1.68 1.68A1.685 1.685 0 0 1 8.43 5.89V1.68C8.43.757 9.186 0 10.11 0c.926 0 1.681.756 1.681 1.68v4.21zm-1.681 6.748c.926 0 1.682.756 1.682 1.681S11.036 16 10.11 16s-1.681-.756-1.681-1.68v-1.682h1.68zm0-.847c-.924 0-1.68-.755-1.68-1.68 0-.925.756-1.681 1.68-1.681h4.21c.924 0 1.68.756 1.68 1.68 0 .926-.756 1.681-1.68 1.681h-4.21z" />
-                </svg>
-              </a>
             </div>
           </div>
 
-          <div class="flex flex-col rounded-xl p-4 md:p-6 bg-white border border-gray-200 dark:bg-slate-900 dark:border-gray-700">
+          <div class="flex flex-col rounded-xl p-4 md:p-6 bg-white border border-gray-200 ">
             <div class="flex items-center gap-x-4">
               <img
                 class="rounded-full w-20 h-20"
@@ -90,9 +76,7 @@ const Alumni = () => {
                 alt="Image Description"
               />
               <div class="grow">
-                <h3 class="font-medium text-gray-800 dark:text-gray-200">
-                  Univer Omoro
-                </h3>
+                <h3 class="font-medium text-gray-800 ">Univer Omoro</h3>
                 <p class="text-xs uppercase text-gray-500">UI/UX Designer</p>
               </div>
             </div>
@@ -104,7 +88,7 @@ const Alumni = () => {
 
             <div class="mt-3 space-x-1">
               <a
-                class="inline-flex justify-center items-center text-gray-500 border border-gray-200 w-8 h-8 rounded-md hover:text-gray-800 hover:shadow-sm dark:hover:text-gray-200 dark:border-gray-700 dark:hover:shadow-slate-700/[.7]"
+                class="inline-flex justify-center items-center text-gray-500 border border-gray-200 w-8 h-8 rounded-md hover:text-gray-800 hover:shadow-sm"
                 href="#"
               >
                 <svg
@@ -119,7 +103,7 @@ const Alumni = () => {
                 </svg>
               </a>
               <a
-                class="inline-flex justify-center items-center text-gray-500 border border-gray-200 w-8 h-8 rounded-md hover:text-gray-800 hover:shadow-sm dark:hover:text-gray-200 dark:border-gray-700 dark:hover:shadow-slate-700/[.7]"
+                class="inline-flex justify-center items-center text-gray-500 border border-gray-200 w-8 h-8 rounded-md hover:text-gray-800 hover:shadow-sm "
                 href="#"
               >
                 <svg
@@ -133,34 +117,17 @@ const Alumni = () => {
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                 </svg>
               </a>
-              <a
-                class="inline-flex justify-center items-center text-gray-500 border border-gray-200 w-8 h-8 rounded-md hover:text-gray-800 hover:shadow-sm dark:hover:text-gray-200 dark:border-gray-700 dark:hover:shadow-slate-700/[.7]"
-                href="#"
-              >
-                <svg
-                  class="w-3.5 h-3.5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M3.362 10.11c0 .926-.756 1.681-1.681 1.681S0 11.036 0 10.111C0 9.186.756 8.43 1.68 8.43h1.682v1.68zm.846 0c0-.924.756-1.68 1.681-1.68s1.681.756 1.681 1.68v4.21c0 .924-.756 1.68-1.68 1.68a1.685 1.685 0 0 1-1.682-1.68v-4.21zM5.89 3.362c-.926 0-1.682-.756-1.682-1.681S4.964 0 5.89 0s1.68.756 1.68 1.68v1.682H5.89zm0 .846c.924 0 1.68.756 1.68 1.681S6.814 7.57 5.89 7.57H1.68C.757 7.57 0 6.814 0 5.89c0-.926.756-1.682 1.68-1.682h4.21zm6.749 1.682c0-.926.755-1.682 1.68-1.682.925 0 1.681.756 1.681 1.681s-.756 1.681-1.68 1.681h-1.681V5.89zm-.848 0c0 .924-.755 1.68-1.68 1.68A1.685 1.685 0 0 1 8.43 5.89V1.68C8.43.757 9.186 0 10.11 0c.926 0 1.681.756 1.681 1.68v4.21zm-1.681 6.748c.926 0 1.682.756 1.682 1.681S11.036 16 10.11 16s-1.681-.756-1.681-1.68v-1.682h1.68zm0-.847c-.924 0-1.68-.755-1.68-1.68 0-.925.756-1.681 1.68-1.681h4.21c.924 0 1.68.756 1.68 1.68 0 .926-.756 1.681-1.68 1.681h-4.21z" />
-                </svg>
-              </a>
             </div>
           </div>
 
           <a
-            class="col-span-full lg:col-span-1 group flex flex-col justify-center text-center rounded-xl p-4 md:p-6 border border-dashed border-gray-200 hover:shadow-sm dark:border-gray-700"
+            class="col-span-full lg:col-span-1 group flex flex-col justify-center text-center rounded-xl p-4 md:p-6 border border-dashed border-gray-200 hover:shadow-sm"
             href="#"
           >
-            <h3 class="text-lg text-gray-800 dark:text-gray-200">
-              Join PLP Academy
-            </h3>
+            <h3 class="text-lg text-gray-800">Join PLP Academy</h3>
             <div>
-              <span class="inline-flex items-center gap-x-2 text-blue-600 group-hover:text-blue-700 dark:text-blue-500 dark:group-hover:text-blue-400">
-                See all opening positions
+              <span class="inline-flex items-center gap-x-2 text-[#C1224F] group-hover:text-[#13ABC4]">
+                Join the next cohort
                 <svg
                   class="w-2.5 h-2.5"
                   width="16"
@@ -217,7 +184,9 @@ const Alumni = () => {
                       <CloseOutlinedIcon />
                     </button>
                   </div>
-                  <div className="p-4 overflow-y-auto"></div>
+                  <div className="p-4 overflow-y-auto">
+                    <Cohort1 />
+                  </div>
 
                   <div className="flex justify-between items-center gap-x-2 py-3 px-4 border-t">
                     <button
