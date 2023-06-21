abb00a13160096dceffe6202784182c029d5275f
Evans-mutuku <evansnyamai98@gmail.com>
2023-06-21 09:35:48 +0300

    added post graduation

--- src/components/StdProfiles/ProfileDash.jsx
+++ src/components/StdProfiles/ProfileDash.jsx
@@ -6,7 +6,7 @@ const ProfileDash = () => {
     <main className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 mx-auto">
       <section className="flex gap-24">
         <ProfileNav />
-        {/* <div className="text-center mt-24">
+        <div className="text-center mt-24">
           <h1 className="text-gray-400 font-extrabold text-5xl mb-10">
             In development
           </h1>
@@ -15,11 +15,11 @@ const ProfileDash = () => {
           <h3 className="mt-10 text-[#13ABC4] font-semibold">
             This Feature will be released in the next few hours
           </h3>
-        </div> */}
-
-        <div className="">
-          <h1 className="font-bold">My Profile</h1>
         </div>
+
+        {/* <div className="">
+          <h1 className="font-bold">My Profile</h1>
+        </div> */}
       </section>
     </main>
   );
