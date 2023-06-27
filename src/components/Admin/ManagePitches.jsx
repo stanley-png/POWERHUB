c361188a2404a4edaf7991510ec6fd0e2ba9edcb
Evans-mutuku <evansnyamai98@gmail.com>
2023-06-27 14:40:23 +0300

    creating admin

--- src/components/Admin/ManagePitches.jsx
+++ src/components/Admin/ManagePitches.jsx
@@ -24,7 +24,23 @@ const ManagePitches = () => {
       ) : (
         <section>
           <AdminNav />
-          <section className="md:mt-16 px-4 sm:px-6 md:mx-8 lg:pl-72"></section>
+          <section className="md:mt-16 px-4 sm:px-6 md:mx-8 lg:pl-72">
+            <header className="max-w-7xl mb-6 flex justify-between">
+              <div>
+                <h1 class="block text-2xl font-bold text-gray-800 sm:text-3xl">
+                  All Pitch Decks
+                </h1>
+              </div>
+              <div>
+                <button
+                  onClick={() => navigate("/Add-Pitch")}
+                  className="py-3 px-9 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-[#13ABC4]  text-white hover:bg-[#C1224F] text-sm "
+                >
+                  Add Project
+                </button>
+              </div>
+            </header>
+          </section>
         </section>
       )}
     </main>
