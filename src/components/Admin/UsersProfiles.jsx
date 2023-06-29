359be43d552ad144ab89b20748dcaa27a62642b7
Evans-mutuku <evansnyamai98@gmail.com>
2023-06-29 16:17:16 +0300

    creating admin

--- src/components/Admin/UsersProfiles.jsx
+++ src/components/Admin/UsersProfiles.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import AdminNav from "./AdminNav";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
 
 const UsersProfiles = () => {
-  return <div>UserProfiles</div>;
+  const user = useSelector(selectUser);
+
+  const authorizedEmails = [
+    "evansnyamai98@gmail.com",
+    "evansifysoftwares@gmail.com",
+    "mungastan@gmail.com",
+  ];
+
+  return (
+    <main>
+      {!user || !authorizedEmails ? (
+        <div className="flex justify-center flex-col text-center min-h-[55vh]">
+          <h1 className="text-2xl font-semibold">
+            You are not authorized to access this page.
+          </h1>
+        </div>
+      ) : (
+        <>
+          <AdminNav />
+          <section className="md:mt-16 px-4 sm:px-6 md:mx-8 lg:pl-72">
+            <section className="max-w-7xl min-h-[70vh]"></section>
+          </section>
+        </>
+      )}
+    </main>
+  );
 };
 
 export default UsersProfiles;
