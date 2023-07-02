982e68dcbf934eae1b4d723ac52b07694409fddb
Evans-mutuku <evansnyamai98@gmail.com>
2023-07-02 17:01:55 +0300

    creating admin

--- src/components/Admin/AdminCreateProfile.jsx
+++ src/components/Admin/AdminCreateProfile.jsx
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const AdminCreateProfile = () => {
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
+  const [fName, setFName] = useState("");
+  const [gender, setGender] = useState("");
+  const [country, setCountry] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [bio, setBio] = useState("");
+  const [cohort, setCohort] = useState("");
+  const [website, setWebsite] = useState("");
+  const [currentActivity, setCurrentActivity] = useState("");
+  const [employment, setEmployment] = useState("");
+  const [career, setCareer] = useState("");
+
+  const [articleImage, setArticleImage] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleImagePreview = (e) => {
+    const file = e.target.files[0];
+    setArticleImage(file);
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setImagePreview(null);
+    }
+  };
   return (
     <section>
       <button
